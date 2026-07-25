38ee207985848d0361dd3c23e5b31948ab9727df
limengosocrate-del <limengosocrate@gmail.com>
2026-07-25 22:12:22 +0100

    Update app.config.ts

--- angularfire-start/src/app/app.config.ts
+++ angularfire-start/src/app/app.config.ts
@@ -1,24 +1,30 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { environment } from '../environments/environment';
-import { provideAuth, getAuth, connectAuthEmulator } from '@angular/fire/auth';
-import { provideFirestore, getFirestore, connectFirestoreEmulator } from '@angular/fire/firestore';
-import { provideFunctions, getFunctions, connectFunctionsEmulator} from '@angular/fire/functions';
-import { provideMessaging, getMessaging } from '@angular/fire/messaging';
-import { provideStorage, getStorage, connectStorageEmulator } from '@angular/fire/storage';
-import { routes } from './app.routes';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideFunctions, getFunctions } from '@angular/fire/functions';
+import { provideMessaging, getMessaging } from '@angular/fire/messaging';
+import { provideStorage, getStorage } from '@angular/fire/storage';
+
+import { environment } from '../environments/environment';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom(
-      provideFirebaseApp(() => initializeApp(environment.firebase)),
-      provideFirestore(() => getFirestore()),
-      provideAuth(() => getAuth()),
-      provideFunctions(() => getFunctions()),
-      provideStorage(() => getStorage()),
-      provideMessaging(() => getMessaging())
-    ),
-    provideRouter(routes)
+    provideRouter(routes),
+
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+
+    provideAuth(() => getAuth()),
+
+    provideFirestore(() => getFirestore()),
+
+    provideFunctions(() => getFunctions()),
+
+    provideStorage(() => getStorage()),
+
+    provideMessaging(() => getMessaging()),
   ],
 };
